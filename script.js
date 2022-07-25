a1f4099dfd77ce54073a1485045e5398cd98d4f3
imLIVI <lesly120898@gmail.com>
2022-07-25 19:35:10 +0300

    Added a theme change

---
+++ script.js
@@ -0,0 +1,15 @@
+let switchMode = document.getElementById("theme");
+
+let imgTheme = document.getElementById("img-theme");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("style-theme");
+
+    if (theme.getAttribute("href") == "css/style-light-theme.css") {
+        theme.href = "css/style-dark-theme.css";
+        imgTheme.src = "/img/icon-dark-theme-moon.png";
+    } else {
+        theme.href = "css/style-light-theme.css";
+        imgTheme.src = "/img/icon-light-theme-sun.png";
+    }
+}
