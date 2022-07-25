e8cf5f8940a90e4cf9c0862c6461b23cf9d9c13c
imLIVI <lesly120898@gmail.com>
2022-07-25 23:14:32 +0300

    Added arrow to top

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let switchMode = document.getElementById("theme");
-
 let imgTheme = document.getElementById("img-theme");
+let imgArrowToTop = document.getElementById("arrow-to-top");
 
 switchMode.onclick = function () {
     let theme = document.getElementById("style-theme");
 
     if (theme.getAttribute("href") == "css/style-light-theme.css") {
         theme.href = "css/style-dark-theme.css";
-        //for gitHub: https://github.com/imLIVI/site-portfolio/img/icon-dark-theme-moon.png
         imgTheme.src = "https://github.com/imLIVI/site-portfolio/blob/gh-pages/img/icon-dark-theme-moon.png?raw=true";
-        
+        imgArrowToTop.src = "img/icon-up-arrow-dark.png";
     } else {
         theme.href = "css/style-light-theme.css";
-        //for gitHub: https://github.com/imLIVI/site-portfolio/img/icon-light-theme-sun.png
         imgTheme.src = "https://github.com/imLIVI/site-portfolio/blob/gh-pages/img/icon-light-theme-sun.png?raw=true";
+        imgArrowToTop.src = "img/icon-up-arrow-light.png";
     }
 }
